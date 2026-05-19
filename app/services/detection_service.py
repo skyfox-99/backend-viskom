4dce18daa54d2b436c702f13ef935b33c57cac43
rivalfitrah <rivalfitrah9@gmail.com>
2026-05-19 13:37:20 +0700

    update cuy

--- app/services/detection_service.py
+++ app/services/detection_service.py
@@ -36,14 +36,12 @@ async def process_image_and_detect(file: UploadFile):
         for box in result.boxes:
             class_name = model.names[int(box.cls[0].item())]
             
-            # Ambil data dari kamus lokal backend
             dict_info = WASTE_DICTIONARY.get(class_name, {})
             
             detections.append({
                 "class_name": class_name,
                 "confidence": round(box.conf[0].item(), 2),
                 "bounding_box": box.xyxy[0].tolist(),
-                # Sisipkan data kamus ke dalam deteksi
                 "nama_sampah": dict_info.get("nama", class_name),
                 "kategori": dict_info.get("kategori", "Unknown"),
                 "cara_buang": dict_info.get("action", "Tidak ada panduan"),
@@ -53,16 +51,15 @@ async def process_image_and_detect(file: UploadFile):
             if class_name not in detected_class_names:
                 detected_class_names.append(class_name)
 
-            if not detections:
-                return {
-                    "status": "not_found",
-                    "message": "Gambar tidak terdeteksi sebagai sampah plastik yang dikenali.",
-                    "detections": [],
-                    "detected_class_names": []
-                }
+        # PASTIKAN BAGIAN INI SEJAJAR DENGAN 'for', BUKAN DI DALAMNYA!
+        if not detections:
+            return {
+                "status": "not_found",
+                "message": "Gambar tidak terdeteksi sebagai sampah plastik yang dikenali.",
+                "detections": [],
+                "detected_class_names": []
+            }
             
-            
-
         img_with_boxes = result.plot()
         filename = f"scan_{uuid.uuid4().hex}.jpg"
         filepath = os.path.join(UPLOAD_DIR, filename)
